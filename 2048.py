7f1087ad5ba05c797bce856933e96271918a83a9
Borovsky0 <borovsky2000@gmail.com>
2023-10-07 15:04:02 +0300

    code improvement and close #11

--- 2048.py
+++ 2048.py
@@ -13,14 +13,12 @@ screen = pygame.display.set_mode(
 pygame.display.set_caption("2048")
 clock = pygame.time.Clock()
 
-'''
 commands = {
-    pygame.K_UP: game.up,
-    pygame.K_DOWN: game.down,
-    pygame.K_LEFT: game.left,
-    pygame.K_RIGHT: game.right
+    pygame.K_UP: "up",
+    pygame.K_DOWN: "down",
+    pygame.K_LEFT: "left",
+    pygame.K_RIGHT: "right"
 }
-'''
 
 check_screen = overlay_screen.OverlayScreen(
     c.WINDOW_WIDTH,
@@ -140,23 +138,10 @@ def run():
             if event.type == pygame.QUIT:
                 game_done = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    done = game.up()
-                    if done: 
+                if event.key in commands:
+                    done = getattr(game, commands[event.key])()
+                    if done:
                         game.add()
-                elif event.key == pygame.K_DOWN:
-                    done = game.down()
-                    if done: 
-                        game.add()
-                elif event.key == pygame.K_LEFT:
-                    done = game.left()
-                    if done: 
-                        game.left()
-                elif event.key == pygame.K_RIGHT:
-                    done = game.right()
-                    if done: 
-                        game.add()
-                        
 
         if draw(game.matrix, game.score):
             check_screen.draw(
